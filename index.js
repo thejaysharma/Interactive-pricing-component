c6c6274a6731b4be963252b9c6fb086ab3cdf7ea
thejaysharma <jai3500000@gmail.com>
2022-12-25 00:00:35 +0530

    dark mode added

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var check = false;
-var slider_value;
+var slider_value = 0;
 function Discount() {
     var checkBox = document.getElementById("myCheck");
     if (checkBox.checked == true) {
@@ -14,14 +14,14 @@ function Discount() {
 function RangeSlider(value) {
     slider_value = value;
 
-    if (check == true){
-        document.getElementById('rangeValue').innerHTML = value-(value*25/100);
+    if (check == true) {
+        document.getElementById('rangeValue').innerHTML = value - (value * 25 / 100);
     }
-    else{
+    else {
         document.getElementById('rangeValue').innerHTML = value;
     }
 
-    var x = value*2.6;
+    var x = value * 2.6;
     var color = 'linear-gradient(90deg, hsl(174, 77%, 80%)' + x + '% , hsl(224, 65%, 95%)' + x + '%)';
     document.getElementById('myrange').style.background = color;
 
@@ -37,4 +37,17 @@ function RangeSlider(value) {
         document.getElementById('pageviews__value').innerHTML = PageViewsValue[value];
     }
 
+};
+
+function SetDarkTheme() {
+    var checkBoxTheme = document.getElementById("darkmode-toggle");
+    if (checkBoxTheme.checked == true) {
+        document.body.classList.add('dark-theme');
+        document.getElementById("bgClickAndChange").src = "./images/bg-pattern-dark.svg";
+        document.getElementById("patternClickAndChange").src = "./images/pattern-circles-dark.png";
+    } else if (checkBoxTheme.checked == false) {
+        document.body.classList.remove('dark-theme');
+        document.getElementById("bgClickAndChange").src = "./images/bg-pattern.svg";
+        document.getElementById("patternClickAndChange").src = "./images/pattern-circles.svg";
+    }
 };
